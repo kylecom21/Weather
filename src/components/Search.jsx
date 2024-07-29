1aa448fa4cf78630987dacc54d726f6bcc3283d0
Kyle Compton <kylecompton@hotmail.co.uk>
2024-07-29 15:35:32 +0100

    extras from last

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react"
+
 export default function Search ({setLocation}) {
+    const [searchInput, setSearchInput] = useState("")
     return (
         <p>Search</p>
     )
--- src/components/WeatherData.jsx
+++ src/components/WeatherData.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react"
+
 export default function WeatherData ({location}) {
+    const [weatherData, setWeatherData] = useState({})
     return (
         <p>Weather Data</p>
     )
